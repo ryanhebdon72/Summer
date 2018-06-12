cb4b72619074bbc95a683dbddeed8b81069dbe36
Grant Innerst <gi7584@gmail.com>
2018-06-12 10:42:19 -0500

    genesis

---
+++ R/add.R
@@ -0,0 +1,14 @@
+#' Adding Two Numbers Together
+#'
+#' This function adds two numbers together in the usual way
+#'
+#' @param a the first number
+#' @param b the second number
+#' @return the summation result
+#' @export add
+#' @examples
+#'
+#' add(2,4)
+#'
+
+add <- function(a,b) a+b
